3d626c34c2b4a0adc517e80ebf721b5e2093ecc8
sandrayeboahAE <sandraeyeboah@gmail.com>
2023-08-24 10:03:29 -0700

    102-print_comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0 ; i <= 99 ; i++)
+	{
+		for (j = 0 ; j <= 99 ; j++)
+		{
+			if (i <= j)
+			{
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(' ');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+
+				if (i < 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
